e09d025cc5f29ed4cd782608098c75873f696232
AgnaldoCordeiro <agske13@gmail.com>
2022-06-13 10:55:49 -0300

    subindo projeto pro github

---
+++ assets/js/js.js
@@ -0,0 +1,35 @@
+const btnAdd = document.getElementById('add-task');
+const list_task = document.getElementById('tasks1');
+
+var id = 0;
+
+function sendTask() {
+  const txt = document.getElementById('new-task-i').value;
+  const text = document.createTextNode(txt);
+
+
+  const TaskContainer = document.createElement('li');
+  const check = document.createElement('input');
+  const label = document.createElement('label');
+
+
+  TaskContainer.classList.add(id);
+  id++;
+
+  label.classList.add('msg');
+
+  check.setAttribute('type', 'checkbox');
+  check.setAttribute('name', txt);
+  check.setAttribute('id', txt);
+
+  label.setAttribute('for', txt);
+  label.appendChild(text);
+
+  TaskContainer.appendChild(check);
+  TaskContainer.appendChild(label);
+
+  list_task.appendChild(TaskContainer);
+
+}
+
+btnAdd.addEventListener("click", sendTask);
